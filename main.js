7282577f2ecdf1cfa7af73090f75033643209ea9
tg0xff <tg0xff@proton.me>
2023-09-05 14:06:35 -0600

    Change #grid-container to a class
    To make it easier to override its declarations.

--- main.js
+++ main.js
@@ -2,18 +2,18 @@ const body = document.querySelector("body");
 
 function makeDivs() {
   const div = document.createElement("div");
-  div.setAttribute("id", "grid-container");
+  div.classList.add("grid-container");
 
   for (let i = 0; i < 256; i++) {
-    let column = document.createElement("div");
-    div.appendChild(column);
+    let square = document.createElement("div");
+    div.appendChild(square);
   }
 
   body.appendChild(div);
 }
 makeDivs();
 
-const divs = document.querySelectorAll("#grid-container div");
+const divs = document.querySelectorAll(".grid-container div");
 divs.forEach((div) => {
   div.addEventListener("mouseover", (e) => {
     e.target.classList.add("hovered");
