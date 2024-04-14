4027eddc74ddce45ec9316594ef05e0e741074ec
tg0xff <tg0xff@proton.me>
2024-04-13 18:20:44 -0600

    Add event listener that changes bg color

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ for (let i = 0; i < 16; i++) {
   const div = document.createElement("div");
   canvas.appendChild(div);
 }
+
+canvas.addEventListener("mouseover", (e) => {
+  if (e.target.parentNode.getAttribute("id") === "canvas") {
+    e.target.style["background-color"] = "black";
+  }
+})
