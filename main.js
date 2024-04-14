837ca3d538d5300a8b1fa1c96c1a807ca5a9b631
tg0xff <tg0xff@proton.me>
2024-04-13 18:38:29 -0600

    Add event listener for the new button

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 const canvas = document.querySelector("#canvas");
-
 canvas.addEventListener("mouseover", (e) => {
   if (e.target.parentNode.getAttribute("id") === "canvas") {
     e.target.classList.toggle("drawn");
   }
 });
+
+const button = document.querySelector("button");
+button.addEventListener("click", (e) => {
+  let newGridSize;
+  let keepGoing = true;
+  while (keepGoing) {
+    newGridSize = prompt("Enter new grid size:");
+    newGridSize = +newGridSize;
+    if (typeof newGridSize === "number" && newGridSize > 0) {
+      keepGoing = false;
+    }
+  }
+  makeCanvasGrid(newGridSize);
+})
 
 function makeCanvasGrid(gridSize) {
   for (let i = 0; i < gridSize; i++) {
