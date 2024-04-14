f595cf065fa421c6da19ec99f871fcd7bbf1919a
tg0xff <tg0xff@proton.me>
2024-04-13 18:23:27 -0600

    Define makeCanvasGrid()

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const canvas = document.querySelector("#canvas");
 
-for (let i = 0; i < 16; i++) {
-  const div = document.createElement("div");
-  canvas.appendChild(div);
-}
-
 canvas.addEventListener("mouseover", (e) => {
   if (e.target.parentNode.getAttribute("id") === "canvas") {
     e.target.style["background-color"] = "black";
   }
 })
+
+function makeCanvasGrid(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
+    const div = document.createElement("div");
+    canvas.appendChild(div);
+  }
+}
+
+makeCanvasGrid(16);
